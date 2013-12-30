b135a41f2f7554114cadf813f6c198366b612974
Martin Akman <martin@akm.com.au>
2013-12-30 15:41:36 +1000

    Change module configuation file from json to php

---
+++ module.php
@@ -0,0 +1,12 @@
+<?php
+$_MODULE = [
+	"name" => "Payment - Test",
+	"description" => "Support for Test payments within the checkout",
+	"namespace" => "\\modules\\payment_test",
+	"config_controller" => "administrator\\PaymentTest",
+	"controllers" => [
+		"administrator\\PaymentTest"
+	],
+	"default_config" => [
+	]
+];
